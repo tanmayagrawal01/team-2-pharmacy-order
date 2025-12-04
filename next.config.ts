d6b90d8d11bcedf7f62e2b43af67fa34f89c157a
Krishna Saraswat <krishna2005saraswat@gmail.com>
2025-12-04 13:15:55 +0530

    added json files

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
